5d05cdc53e7748cb957e33e6a61d2ca28603813e
ArnoSlb <soulabaille.arnaud@gmail.com>
2024-01-22 16:47:53 +0100

    feat(Sidebar): create Sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sidebar = () => {
+
+    return (
+        <div className="Sidebar">
+            <h2 className="titleSidebar">Chaines recommandées</h2>
+            <ul className="listStream">
+                
+            </ul>
+        </div>
+    )
+};
+
+export default Sidebar;

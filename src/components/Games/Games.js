94f66b20c2b52b42d382f2fc173a9c5c4f5c19d2
ArnoSlb <soulabaille.arnaud@gmail.com>
2024-01-26 11:10:55 +0100

    feat(add games): add games with the api call and map on each element

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -10,6 +10,17 @@ const Games = () => {
         const fetchData = async () => {
             const result = await api.get('https://api.twitch.tv/helix/games/top')
             console.log(result);
+
+            let dataArray = result.data.data;
+            let finalArray = dataArray.map((game) => {
+                let newUrl = game.box_art_url
+                .replace("{width}", "250")
+                .replace("{height}", "300");
+            game.box_art_url = newUrl;
+            return game;
+            });
+
+            setGames(finalArray);
         };
 
         fetchData();
@@ -19,7 +30,15 @@ const Games = () => {
         <div>
             <h1 className="titleGames">Jeux les plus populaires</h1>
             <div className="flexAccueil">
-
+                {games.map((game, index) => (
+                    <div key={index} className="cardGames">
+                        <img src={game.box_art_url} alt="jeu profile pic" className="imgCard" />
+                        <div className="cardBodyGames">
+                            <h5 className="titleCardGames">{game.name}</h5>
+                            <div className="btnCard">Regarder {game.name}</div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
